1d3ff2851aec33834b7efeab9c5e97633db0fe97
张楠 <zhangnan@local.com>
2019-06-24 09:08:48 +0800

    修改获取我的任务丢失候选人任务

--- NActiviti/Sys.Bpm.Engine/Engine/impl/cmd/GetMyTasksCmd.cs
+++ NActiviti/Sys.Bpm.Engine/Engine/impl/cmd/GetMyTasksCmd.cs
@@ -42,7 +42,7 @@ namespace Sys.Workflow.Engine.Impl.Cmd
             var taskService = commandContext.ProcessEngineConfiguration.TaskService;
 
             List<ITask> tasks = taskService.CreateTaskQuery()
-                .SetTaskAssignee(assignee)
+                .SetTaskCandidateOrAssigned(assignee)
                 .OrderByTaskCreateTime()
                 .Desc()
                 .List()
